3ef1eb987bd30a5e2f1ff6c03597836b81953691
dillardt <80779970+dillardt@users.noreply.github.com>
2022-11-11 10:32:02 -0600

    Basic Distance Finder, Database changes
    Basic distance as 3d distance with rgb, and fixed hex codes to fit with python ones

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import colormath
 import pandas
 
-class color():
-    def __init__(self, name, parent_name, rgb, hex_code):
-        self.name = name
-        self.parent_name = parent_name
-        self.rgb = rgb
-        self.hex_code = hex_code
-    
-df = pandas.read_csv("colordata.csv")
-print(df)
+def color_distance(color1, color2):
+    return ((color2[0]-color1[0])**2 + (color2[1]-color1[1])**2 + (color2[2]-color1[2])**2)**0.5
+
+colors = pandas.read_csv("colordata.csv")
+colors_len = len(colors)
+for i in range(colors_len):
+    colors.iloc[i]["Rgb"] = tuple(eval(colors.iloc[i]["Rgb"]))
+
+def find_closest(start_color):
+    min_color = 0
+    min_color_distance = color_distance(start_color, colors.iloc[min_color]["Rgb"])
+    for i in range(1,colors_len):
+        current_distance = color_distance(start_color, colors.iloc[i]["Rgb"])
+        if current_distance < min_color_distance:
+            min_color = i
+            min_color_distance = current_distance
+    return min_color
+
+print(colors.iloc[find_closest((100,100,100))])
