d8b1c32aee3dcd0521391aa02d73aa84f1207b7c
dillardt <80779970+dillardt@users.noreply.github.com>
2022-11-14 21:52:58 -0600

    Major Functionality with Delta E2000 function
    Borrowed code from GitHub for Delta E2000 and I added the ability to grab an image from the clipboard and get the color values from it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas
+from PIL import Image, ImageGrab, ImageStat
 from color_stuffs import *
 
 
@@ -10,12 +11,16 @@ for i in range(colors_len):
 
 def find_closest(start_color):
     min_color = 0
-    min_color_distance = color_distance(start_color, colors.iloc[min_color]["Lab"])
+    min_color_distance = CIEDE2000(start_color, colors.iloc[min_color]["Lab"])
     for i in range(1,colors_len):
-        current_distance = color_distance(start_color, colors.iloc[i]["Lab"])
+        current_distance = CIEDE2000(start_color, colors.iloc[i]["Lab"])
         if current_distance < min_color_distance:
             min_color = i
             min_color_distance = current_distance
     return min_color
 
-print(rgb_to_lab((100,100,100)))
+pic = ImageGrab.grabclipboard()
+average_lab = rgb_to_lab(ImageStat.Stat(pic).mean[:-1])
+
+print(average_lab)
+print(colors.iloc[find_closest(average_lab)])
